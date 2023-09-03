45a912fcd72e3261c3eb85e31a012887d6705468
NatanDuarte <natanprocopio@gmail.com>
2023-09-03 16:06:36 -0300

    docs: creating swagger route

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 routes(app);
 
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Running at ${port}`));
+app.listen(port, () => console.log(`Running at http://localhost:${port}/api-docs`));
