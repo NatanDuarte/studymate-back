9e4a411e48ac1657328c0b8afcad8cf621e4ae96
NatanDuarte <natanprocopio@gmail.com>
2023-11-06 20:53:51 -0300

    qualquer coisa ai

--- src/adapters/socketAdapter.js
+++ src/adapters/socketAdapter.js
@@ -1,5 +1,7 @@
 const { Server } = require('socket.io');
 
+const users = {}
+
 const socketAdapter = (server) => {
     const io = new Server(server, {
         cors: {
@@ -8,14 +10,40 @@ const socketAdapter = (server) => {
     });
 
     io.on('connection', (socket) => {
-        socket.on('room_selected', (roomId) => {
+        const sendChatMessage = ({ message, username, roomId }) => {
+            io.to(roomId).emit('message', { username, message });
+        }
+
+        const joinRoom = ({ roomId, userId }) => {
             socket.join(roomId);
+
+            if (!users[roomId]) {
+                users[roomId] = [];
+            }
+            users[roomId].push(userId);
+            socket.to(roomId).emit('users-connected', users[roomId]);
+
+            socket.on("disconnect", () => {
+                console.log("user disconnected ", userId);
+                leaveRoom({ roomId, userId });
+            });
+        }
+
+        const leaveRoom = ({ roomId, userId }) => {
+            socket.to(roomId).emit("user-disconnected", userId);
+            users[roomId] = users[roomId]?.filter((id) => id !== userId);
+        };
+
+        socket.on('get-users', ({ roomId }) => {
+            console.log('ARRAY OF ALL USERS', users);
+            console.log('ARRAY OF USERS', users[roomId]);
+            console.log('ARRAY OF USERS', typeof (users[roomId]));
+            socket.to(roomId).emit('users-connected', { users: users[roomId] });
         });
 
-        socket.on('message', (data) => {
-            const { message, username, roomId } = data;
-            io.to(roomId).emit('message', { username, message });
-        });
+
+        socket.on('join-video-sharing', joinRoom);
+        socket.on('message', sendChatMessage);
     });
 
     return io;
